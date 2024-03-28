204afbdc5af9b030d3a2ab559daef1db9a2f4a52
juergen <juergen.porbadnigk@nagold.de>
2024-03-28 16:22:52 +0100

    [upd] Add scanStateEvent

---
+++ lib/Event/ScanStateEvent.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @copyright Copyright (c) 2022 Alexander Volz <gh-contact@volzit.de>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\Files_Staticmimecontrol\Event;
+
+use OCP\EventDispatcher\Event;
+
+class ScanStateEvent extends Event {
+        /** @var bool */
+        private $state;
+
+        public function __construct(bool $state) {
+                parent::__construct();
+                $this->state = $state;
+        }
+
+        public function getState(): bool {
+                return $this->state;
+        }
+}
